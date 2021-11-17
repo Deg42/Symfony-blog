dc3ab4e26a56981786e078f03e3d3b3916de8271
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2021-11-17 10:30:56 +0100

    Update noticias to DB

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -13,7 +13,15 @@ class BlogController extends AbstractController
 {
     function index()
     {
-        return $this->render('blog/index.html.twig');
+        // Obtenemos el gestor de entidades de Doctrine
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // obtenemos todas las apuestas
+        $noticias = $entityManager->getRepository(Noticia::class)->findAll();
+
+        return $this->render('blog/index.html.twig', array(
+            'noticias' => $noticias,
+        ));
     }
 
     function verNoticia($id)
@@ -22,24 +30,69 @@ class BlogController extends AbstractController
         if ((string)(int) $id != $id) {
             return $this->redirectToRoute('index');
         }
+        // Obtenemos el gestor de entidades de Doctrine
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        /* Obtenenemos el repositorio de Noticias y
+           buscamos en el usando la id de la noticia */
+        $noticia = $entityManager->getRepository(Noticia::class)->find($id);
+
+        // Si la apuesta no existe lanzamos una excepción.
+        if (!$noticia) {
+            throw $this->createNotFoundException(
+                'No existe ninguna noticia con id ' . $id
+            );
+        }
 
         return $this->render('blog/noticia.html.twig', [
-            'id' => $id,
+            'noticia' => $noticia,
         ]);
     }
 
-    function nuevaNoticia(){
+    function nuevaNoticia(Request $request)
+    {
         $noticia = new Noticia();
         $form = $this->createFormBuilder($noticia)
             ->add('titulo', TextType::class)
             ->add('entradilla', TextareaType::class, array('required' => false))
             ->add('cuerpo', TextareaType::class, array('required' => false))
-            ->add('save', SubmitType::class,
-                array('label' => 'Añadir Noticia'))
+            ->add(
+                'save',
+                SubmitType::class,
+                array('label' => 'Añadir Noticia')
+            )
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // De esta manera podemos rellenar la variable
+            // $noticia con los datos del formulario.
+            $noticia = $form->getData();
+
+            // Obtenemos el gestor de entidades de Doctrine
+            $entityManager = $this->getDoctrine()->getManager();
+
+            // Le decimos a doctrine que nos gustaría almacenar
+            // el objeto de la variable en la base de datos
+            $entityManager->persist($noticia);
+
+            // Ejecuta las consultas necesarias
+            $entityManager->flush();
+
+            //Redirigimos a una página de confirmación.
+            return $this->redirectToRoute('noticiaCreada');
+        }
+
 
         return $this->render('blog/nuevaNoticia.html.twig', array(
             'form' => $form->createView(),
         ));
     }
+
+    public function noticiaCreada()
+    {
+        return $this->render('blog/noticiaCreada.html.twig');
+    }
+
 }
